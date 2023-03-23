3247d921eb45bcb91693b2c823d8a3ade14c23b5
Raul Rueda <raul@raulrueda.dev>
2023-03-23 00:53:16 -0600

    Alive root endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,13 @@
-//Placeholder
+// External lib
+const express = require("express");
+const cors = require("cors");
+
+// Routes
+const root = require("./alive");
+
+const app = express();
+app.use(cors());
+
+app.use("/", root);
+
+module.exports = app;
